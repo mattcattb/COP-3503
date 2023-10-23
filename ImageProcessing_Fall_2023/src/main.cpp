e075dc7451875ffc64c4d4d6a0ee5761af98663b
mattcattb <mboughton17@gmail.com>
2023-10-23 18:04:24 -0400

    finished a basic command  line interface for project

--- ImageProcessing_Fall_2023/src/main.cpp
+++ ImageProcessing_Fall_2023/src/main.cpp
@@ -10,96 +10,93 @@ const string method_names[] = {"multiply", "subtract", "overlay", "screen", "com
 
 const int num_args_method[] = {1, 1, 1, 1, 2, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1}; // number of arguments used not including method name itself
 
-Image multiply(Image top, string * args);
-Image subtract(Image top, string * args);
-Image overlay(Image top, string * args);
-Image screen(Image top, string * args);
-Image combine(Image top, string * args);
-Image flip(Image top, string * args);
+Image multiply(Image top, Image bottom);
+Image subtract(Image top, Image bottom);
+Image overlay(Image top, Image bottom);
+Image screen(Image top, Image bottom);
+Image combine(Image top, Image green, Image blue);
+Image flip(Image top);
 
-Image onlyred(Image top, string * args);
-Image onlyblue(Image top, string * args);
-Image onlygreen(Image top, string * args);
+Image onlyred(Image top);
+Image onlyblue(Image top);
+Image onlygreen(Image top);
 
-Image addred(Image top, string * args);
-Image addgreen(Image top, string * args);
-Image addblue(Image top, string * args);
+Image addred(Image top, int val);
+Image addgreen(Image top, int val);
+Image addblue(Image top, int val);
 
-Image scalered(Image top, string * args);
-Image scalegreen(Image top, string * args);
-Image scaleblue(Image top, string * args);
+Image scalered(Image top, int scale);
+Image scalegreen(Image top, int scale);
+Image scaleblue(Image top, int scale);
 
 void display_help_message();
 
 bool is_tga(string filename);
 bool file_exists(string filename);
-int get_method_id(string method_name);
+bool args_valid(int method_id, int i, char ** argv, int argc);
 
-void function_multiplexer(int method_id, string * args, Image &tracking);
+int get_method_id(string method_name);
 string * parse_args(int method_id, int index, char** argv);
 
-//! move this outside of src directory!
+void function_multiplexer(int method_id, string * args, Image &tracking);
+
 int main(int argc, char** argv) {
 
-    if (argv[0] == "--help" || argc == 0){
+    // display message
+    if (argv[1] == "--help" || argc == 1){
         // print help message if argv[0] is --help or there are no arguements provided
         display_help_message();
     }
 
-    // check if argv[0] is tga filename
-
-    string output_file = argv[0];
+    string output_file = argv[1];
 
     if (is_tga(output_file) == false){
         // stop if tga is not a file
-        cout << "Invalid file name."<< endl;
         return 1;
     }
 
-    string source_file = argv[1];
+    string source_file = argv[2];
     
     if(is_tga(source_file) == false){
-        cout << "Invalid file name." << endl;
         return 1;
     
     } else if(file_exists(source_file) == false){
-        cout << "File does not exist." << endl;
         return 1;
     }  
 
     Image tracking_img(source_file);
     tracking_img.read();
 
-    int i = 2; // start at 2 for output_file and source_file
+    int i = 3; // start at 2 for output_file and source_file
 
     while (i < argc){
         string cur_method = argv[i]; // current method
         int method_id = get_method_id(cur_method);
-        
+
+        cout << "i: " << i << " method: " << cur_method << endl;
+
         if (method_id == -1){
-            //! wrong method ID!!!
             cout << "Invalid method name." << endl;
             return 1;
         }        
     
-        // see if arguements are missing or not?
+        // determine if args are valid
         int num_args = num_args_method[method_id];
+        if (args_valid(method_id, i, argv, argc) == false){
+            return 1;
+        }
 
         // get string of args for the function. 
-        //! THIS WILL ONLY WORK if argv array has enough arguements
         string *method_args = parse_args(method_id, i, argv);
-
-        // arguement check
-        for(int i = 0; i < num_args; i += 1){
-            string cur_arg = method_args[i];
-
-        }
 
         // apply that function and arguement to the function multiplexer!
         function_multiplexer(method_id, method_args, tracking_img);
+        
 
+        delete[] method_args;
         // after everything done, incriment according to which method used
-        i += (1 + num_args);
+        i += 1;
+        i += num_args;
     }
 
     tracking_img.write(output_file);
@@ -107,16 +104,56 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-bool args_valid(int method_id, string * args){
-    //! WORK ON THIS FUNCTION maybe?
+bool args_valid(int method_id, int i, char ** argv, int argc){
     // determine if args are valid or not!
     // if file doesnt exist, "Invalid arguement, file does not exist."
     // if arg isnt number, "Invalid arguement, expected number."
     
+    int num_args = num_args_method[method_id];
+
+    if (num_args + i > argc - 1){
+        // missing args
+        cout <<  "Missing argument." << endl;
+        return false;
+    }
+
     if (method_id >= 0 && method_id <=3){
         // args should just be 1 arguement!
-    } 
-    
+        string file_1 = argv[i + 1];
+        if (is_tga(file_1) == false || file_exists(file_1) == false){
+            return false;
+        }
+
+
+    }else if (method_id == 4){
+        // 2 args, 2 filenames
+        string file_1 = argv[i + 1];
+        string file_2 = argv[i + 2];
+
+        if (is_tga(file_1) == false || file_exists(file_1) == false){
+            return false;
+        }else if (is_tga(file_2) == false || file_exists(file_2) == false){
+            return false;
+        }
+
+    }else if (method_id >= 5 && method_id <= 8){
+        // 0 args!
+        return true;
+
+    }else if (method_id >= 9 && method_id <= 14){
+        // 1 arguement that is an integer
+        
+        int val1;
+        try {
+            val1 = stoi(argv[i + 1]);
+        }catch(invalid_argument e){
+            cout << "Invalid arguement, expected number." << endl;
+            return false;
+        }
+
+    }
+
+    return true;
 }
 
 void display_help_message(){
@@ -131,10 +168,12 @@ bool is_tga(string filename){
     // determines if filename ends in .tga
     int len = filename.length();
 
-    string filetype = filename.substr(len-5, 4);
-    if (filetype != ".tga"){
+    string filetype = filename.substr(len-4, 4);
+    if (filetype != ".tga"){    
+        cout << "Invalid file name."<< endl;
         return false;
     }
+
     return true;
 }
 
@@ -144,12 +183,17 @@ bool file_exists(string filename){
     ifstream stream = ifstream(filename, ios_base::binary);
     bool exists = stream.is_open();
     stream.close();
+    if (exists == false){
+        cout << "File does not exist." << endl;
+
+    }
+
     return exists;
 }
 
 int get_method_id(string method_name){
     // return number corresponding to method ID, or -1 if method_name is not valid method
-    for (int i = 0; i < method_names->size(); i += 1){
+    for (int i = 0; i < 15; i += 1){
         if (method_names[i] == method_name){
             return i;
         }
@@ -165,26 +209,26 @@ void function_multiplexer(int method_id, string * args, Image &tracking){
     if (method_id == 0) {
         // multiply 
         string bottom_fname = args[0];
-        Image bottom(bottom_fname);
-        bottom.write();
+        Image bottom;
+        bottom.read(bottom_fname);
         new_img = multiply(tracking, bottom);
     } else if (method_id == 1) {
         // subtract
         string bottom_fname = args[0];
         Image bottom(bottom_fname);
-        bottom.write();
+        bottom.read();
         new_img = subtract(tracking, bottom);
     } else if (method_id == 2) {
         // overlay
         string bottom_fname = args[0];
         Image bottom(bottom_fname);
-        bottom.write();
+        bottom.read();
         new_img = overlay(tracking, bottom);
     } else if (method_id == 3) {
         // screen
         string bottom_fname = args[0];
         Image bottom(bottom_fname);
-        bottom.write();
+        bottom.read();
         new_img = screen(tracking, bottom);
     } else if (method_id == 4) {
         // combine
@@ -192,8 +236,8 @@ void function_multiplexer(int method_id, string * args, Image &tracking){
         string blue_fname = args[1];
         Image green(green_fname);
         Image blue(blue_fname);
-        green.write();
-        blue.write();
+        green.read();
+        blue.read();
         new_img = combine(tracking, green, blue);
     } else if (method_id == 5) {
         // flip
@@ -244,19 +288,22 @@ string * parse_args(int method_id, int index, char** argv){
     // index: index currently on (function itself)
     int num_args = num_args_method[method_id];
 
-    string arguements[num_args];
+    string *arguements = new string[num_args];
 
     for (int i = 0; i < num_args; i += 1){
-        arguements[i] = argv[index + i + 1];
+        
+        string cur_arg = argv[index + i + 1];
+        cout << cur_arg << endl;
+        arguements[i] = cur_arg;
     }
 
     return arguements;
 }
 
-//!!!!! CHANGE EACH OF THESE FUNCTIONS TO TAKE A STRING* OF ARGS!!!
-
 Image multiply(Image top, Image bottom){
-    return (top * bottom);
+    
+    Image return_img = top * bottom;
+    return (return_img);
 }
 
 Image subtract(Image top, Image bottom){
